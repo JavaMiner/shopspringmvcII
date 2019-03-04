d8a0e0c9befa431bd68b1fee61543cdc1f579730
klimas7 <klimas7@gmail.com>
2019-03-04 23:26:07 +0100

    #3 A simple page

---
+++ src/main/java/pl/sii/shopsmvc/WebController.java
@@ -0,0 +1,29 @@
+package pl.sii.shopsmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WebController {
+
+    @ModelAttribute("my_message")
+    public String myMessage() {
+        return "#1: Hi Sii";
+    }
+
+    @RequestMapping("/hi")
+    public String home(Model model){
+        model.addAttribute("my_message2", "#2: hi Sii");
+        return "homePage";
+    }
+
+    @RequestMapping("/hi2")
+    public ModelAndView hi() {
+        ModelAndView modelAndView = new ModelAndView("homePage");
+        modelAndView.addObject("my_message2", "#2: Hi All");
+        return modelAndView;
+    }
+}
