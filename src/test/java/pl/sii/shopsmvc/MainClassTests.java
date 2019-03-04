28018fa07495530f69135886d224f6e5b65ee652
klimas7 <klimas7@gmail.com>
2019-03-04 22:47:41 +0100

    #2 Setup a initial application

---
+++ src/test/java/pl/sii/shopsmvc/MainClassTests.java
@@ -0,0 +1,15 @@
+package pl.sii.shopsmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MainClassTests {
+    @Test
+    public void contextTest() {
+
+    }
+}
