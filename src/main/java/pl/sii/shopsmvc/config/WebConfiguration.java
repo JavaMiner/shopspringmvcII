9153d679519335cde03bfc882b60d9ee1ab8e7d5
klimas7 <klimas7@gmail.com>
2019-03-06 22:54:28 +0100

    #11 Add date format

--- src/main/java/pl/sii/shopsmvc/config/WebConfiguration.java
+++ src/main/java/pl/sii/shopsmvc/config/WebConfiguration.java
@@ -4,11 +4,15 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import pl.sii.shopsmvc.date.USLocaleDateFormatter;
+
+import java.time.LocalDate;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
@@ -16,6 +20,11 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatterForFieldType(LocalDate.class, new USLocaleDateFormatter());
     }
 
     @Bean
---
+++ src/main/java/pl/sii/shopsmvc/date/USLocaleDateFormatter.java
@@ -0,0 +1,31 @@
+package pl.sii.shopsmvc.date;
+
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class USLocaleDateFormatter implements Formatter<LocalDate> {
+    private static final String US_PATTERN = "MM/dd/yyyy";
+    private static final String NORMAL_PATTERN = "dd-MM-yyyy";
+
+    @Override
+    public LocalDate parse(String text, Locale locale) throws ParseException {
+        return LocalDate.parse(text, DateTimeFormatter.ofPattern(getPattern(locale)));
+    }
+
+    @Override
+    public String print(LocalDate object, Locale locale) {
+        return DateTimeFormatter.ofPattern(getPattern(locale)).format(object);
+    }
+
+    private String getPattern(Locale locale) {
+        return isUnitedState(locale) ? US_PATTERN : NORMAL_PATTERN;
+    }
+
+    private boolean isUnitedState(Locale locale) {
+        return Locale.US.getLanguage().equals(locale.getLanguage());
+    }
+}
