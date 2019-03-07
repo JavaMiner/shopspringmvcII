d0699415f6bcdb09431c3c0ddb1da74608ac74e7
klimas7 <klimas7@gmail.com>
2019-03-07 22:04:30 +0100

    #29 Add security and role

--- src/main/java/pl/sii/shopsmvc/config/WebSecurityConfiguration.java
+++ src/main/java/pl/sii/shopsmvc/config/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package pl.sii.shopsmvc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,19 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN")
                 .and()
                 .withUser("user").password(passwordEncoder().encode("user")).roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/orders/**").hasRole("USER")
+                    .antMatchers("/products/**").hasRole("ADMIN")
+                    .antMatchers("/swagger-ui.html").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().and()
+                .httpBasic();
     }
 
     @Bean
