71605c490ea3af364accedf2617079fc4bbfa0eb
klimas7 <klimas7@gmail.com>
2019-03-07 15:08:12 +0100

    #27 check role

--- src/main/java/pl/sii/shopsmvc/controller/HomeController.java
+++ src/main/java/pl/sii/shopsmvc/controller/HomeController.java
@@ -1,12 +1,24 @@
 package pl.sii.shopsmvc.controller;
 
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String home() {
-        return "redirect:/products";
+    public String home(Principal principal) {
+        if (hasRole(principal, "ROLE_ADMIN")) {
+            return "redirect:/products";
+        }
+        else {
+            return "redirect:/orders";
+        }
+    }
+
+    private boolean hasRole(Principal principal, String role) {
+        return ((AbstractAuthenticationToken)principal).getAuthorities().stream().allMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(role));
     }
 }
